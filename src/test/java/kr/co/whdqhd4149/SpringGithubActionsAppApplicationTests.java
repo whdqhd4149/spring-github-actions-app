1be40665f1be16fc7bec6a6fde9e1913e6cf6657
whdqhd4149 <whdqhd4149@gmail.com>
2025-09-26 10:10:29 +0900

    commit 0.0.1

---
+++ src/test/java/kr/co/whdqhd4149/SpringGithubActionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.whdqhd4149;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
